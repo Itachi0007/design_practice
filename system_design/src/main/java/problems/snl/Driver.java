4f46a562ff2dfb8c4b138538c714c322671788d9
Ahmer Mehmood <ahmermehmood@Ahmers-Laptop.local>
2024-12-11 20:46:53 +0530

    Added prompt

--- system_design/src/main/java/problems/snl/Driver.java
+++ system_design/src/main/java/problems/snl/Driver.java
@@ -4,6 +4,7 @@ public class Driver {
     public static void main(String[] args) {
 
         // Using Console to input data from user
+        System.out.println("Enter number of rolls: ");
         Integer snakesCount = Integer.valueOf(System.console().readLine());
         for(int i=0;i<snakesCount;i++) {
             // for now just rolling the dice N times
