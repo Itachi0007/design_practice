ceb6d79867e1d4171e8cb0d92d551530900b7264
ahmermehmood <ahmer.mehmood191@gmail.com>
2024-12-19 15:49:31 +0530

    using scanner class

--- system_design/src/main/java/problems/snl/Driver.java
+++ system_design/src/main/java/problems/snl/Driver.java
@@ -1,11 +1,15 @@
 package problems.snl;
 
+import java.util.Scanner;
+
 public class Driver {
     public static void main(String[] args) {
 
+        System.out.println("Enter number of rolls - ");
         // Using Console to input data from user
-        System.out.println("Enter number of rolls: ");
-        Integer snakesCount = Integer.valueOf(System.console().readLine());
+        Scanner scan = new Scanner(System.in);
+        int snakesCount = scan.nextInt();
+
         for(int i=0;i<snakesCount;i++) {
             // for now just rolling the dice N times
             System.out.println(Dice.getInstance().roll());
