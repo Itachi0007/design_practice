d8d7b837c3c23cae4722022ed43f0c43cdf4be60
Ahmer Mehmood <ahmer.mehmood191@gmail.com>
2025-01-06 23:56:37 +0530

    Added Spring project structure

--- system_design/src/main/java/problems/snl/SnakesNLadderService.java
+++
@@ -1,5 +0,0 @@
-package problems.snl;
-
-public class SnakesNLadderService {
-
-}
--- system_design/src/main/java/problems/wallet_design/Driver.java
+++
@@ -1,7 +0,0 @@
-package problems.wallet_design;
-
-public class Driver {
-    public static void main(String[] args) {
-
-    }
-}
